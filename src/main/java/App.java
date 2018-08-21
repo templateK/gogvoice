63d71a880ee634ab67e9bfac5c027629ba5b145e
templateK <templateK@github.com>
2018-08-21 11:15:55 +0900

    fix indentation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,41 +28,41 @@ public class App {
     // Instantiates a client
     try ( SpeechClient speechClient = SpeechClient.create()) {
 
-      // The path to the audio file to transcribe
-      String fileName = args[0];
+        // The path to the audio file to transcribe
+        String fileName = args[0];
 
-      // Reads the audio file into memory
-      Path path = Paths.get(fileName);
-      byte[] data = Files.readAllBytes(path);
-      ByteString audioBytes = ByteString.copyFrom(data);
-
-
-
-      System.out.println(" Builds the sync recognize request");
-
-      RecognitionConfig config = RecognitionConfig.newBuilder()
-          .setEncoding(AudioEncoding.LINEAR16)
-          .setSampleRateHertz(16000)
-          .setLanguageCode("en-US")
-          .build();
-
-      System.out.println(" Builds the audio");
-      RecognitionAudio audio = RecognitionAudio.newBuilder()
-          .setContent(audioBytes)
-          .build();
+        // Reads the audio file into memory
+        Path path = Paths.get(fileName);
+        byte[] data = Files.readAllBytes(path);
+        ByteString audioBytes = ByteString.copyFrom(data);
 
 
-      System.out.println("Performs speech recognition on the audio file");
 
-      RecognizeResponse response = speechClient.recognize(config, audio);
-      List<SpeechRecognitionResult> results = response.getResultsList();
+        System.out.println(" Builds the sync recognize request");
 
-      for (SpeechRecognitionResult result : results) {
-        // There can be several alternative transcripts for a given chunk of speech. Just use the
-        // first (most likely) one here.
-        SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-        System.out.printf(" Transcription: %s%n", alternative.getTranscript());
-      }
+        RecognitionConfig config = RecognitionConfig.newBuilder()
+            .setEncoding(AudioEncoding.LINEAR16)
+            .setSampleRateHertz(16000)
+            .setLanguageCode("en-US")
+            .build();
+
+        System.out.println(" Builds the audio");
+        RecognitionAudio audio = RecognitionAudio.newBuilder()
+            .setContent(audioBytes)
+            .build();
+
+
+        System.out.println("Performs speech recognition on the audio file");
+
+        RecognizeResponse response = speechClient.recognize(config, audio);
+        List<SpeechRecognitionResult> results = response.getResultsList();
+
+        for (SpeechRecognitionResult result : results) {
+            // There can be several alternative transcripts for a given chunk of speech. Just use the
+            // first (most likely) one here.
+            SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+            System.out.printf(" Transcription: %s%n", alternative.getTranscript());
+        }
     }
   }
 }
